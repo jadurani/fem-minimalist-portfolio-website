f295e67fa5b26f742a1a9de3461031ac8196fc26
Jadurani Davalos <jadurani.davalos@gmail.com>
2024-01-27 18:47:40 -0500

    build(storybook): resolve aliases

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
+const path = require("path");
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -14,6 +15,21 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    // @ts-ignore: Object is possibly 'null'.
+    config.resolve.alias = {
+      // @ts-ignore: Object is possibly 'null'.
+      ...config.resolve.alias,
+      "@assets": path.resolve(__dirname, "../public/"),
+      "@app": path.resolve(__dirname, "../src/app/"),
+      "@api": path.resolve(__dirname, "../src/app/api"),
+      "@tests": path.resolve(__dirname, "../src/tests/"),
+      "@components": path.resolve(__dirname, "../src/components/"),
+      "@lib": path.resolve(__dirname, "../src/lib/"),
+      "@styles": path.resolve(__dirname, "../src/styles/"),
+    };
+    return config;
   },
 };
 export default config;
