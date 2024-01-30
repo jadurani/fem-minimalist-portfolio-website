ba17be35704ac19208d5e24e5dfeb6ca69424422
Jadurani Davalos <jadurani.davalos@gmail.com>
2024-01-30 16:27:22 -0500

    feat: prelim interested section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,18 @@ export default function Home() {
           <Button label="Go to Portfolio" handleClick={() => {}}></Button>
         </article>
       </section>
-      {/* <section>Interested</section> */}
+
+      <section className="py-8">
+        <div className="flex flex-col gap-8 md:flex-row md:justify-between">
+          <p className="text-center md:w-1/2 md:text-left font-heading text-h2 font-bold leading-none md:text-h1">
+            Interested in doing a project together?
+          </p>
+
+          <div className="flex justify-center md:justify-end items-center md:w-1/2">
+            <Button label="Contact Me" handleClick={() => {}}></Button>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
