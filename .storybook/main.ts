97a65ee2d407fa0cfe197dd9df331e55abbfb232
Jadurani Davalos <jadurani.davalos@gmail.com>
2024-01-31 20:31:51 -0500

    feat: footer component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 const path = require("path");
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  staticDirs: ['../public'],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
